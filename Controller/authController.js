81b0cbd1bcc2f3b0745790311c24e7010310192c
Rabbi Hossain <rabbihossainlhp@gmail.com>
2025-01-29 13:07:19 +0600

    initial login page

--- Controller/authController.js
+++ Controller/authController.js
@@ -5,8 +5,10 @@ const user = require("../Models/User");
 
 //signup_route_controll
 exports.signupGetController = (req,res,next)=>{
-    res.render("Pages/auth/signup");
+    res.render("Pages/auth/signup",{currentPage:"Signup"});
+    next();
 }
+
 
 exports.signupPostController = async(req,res,next)=>{
 
@@ -24,7 +26,7 @@ exports.signupPostController = async(req,res,next)=>{
 
         let createdUser = await User.save();
         console.log(createdUser);
-        res.render("Pages/auth/signup");
+        res.render("Pages/auth/login",{currentPage:"Login"});
 
     }catch(er){
         console.log("Something went wrong to create user",er);
@@ -35,14 +37,25 @@ exports.signupPostController = async(req,res,next)=>{
 
 
 
+
+
+
 //login-route_controll
 exports.loginGetController = (req,res,next)=>{
-
+    res.render("Pages/auth/login",{currentPage:"Login"});
+    next();
 }
+
+
+
 
 exports.loginPostController = (req,res,next)=>{
+    
 
+    next();
 }
+
+
 
 
 //logout-route_controll
